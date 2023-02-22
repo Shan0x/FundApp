832d19c165f6a521ab7d1bf95b29de95b682dbca
Katzchii <nguyenhamo98@gmail.com>
2023-02-22 02:29:54 -0600

    view landing and login pages
    - Added a new model for the users table (based on Cris's design)
    -Added the User model to FundFriendContext
    -Added "/api/users" to setup.Proxy.js
    -Added a controller for landing page
    -Added a controller for login page with simple validation method

---
+++ Models/Users.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FundApp.Models
+{
+    public class Users
+    {
+        [Key]
+        public int userid { get; set; }
+        public string? username { get; set; }
+        public string? userpass { get; set; }
+    }
+}
