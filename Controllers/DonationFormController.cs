8c48d6aa10a1efc9479948f6f00b1a72862af0f3
crjpb3 <60162347+crjpb3@users.noreply.github.com>
2023-02-27 22:16:01 -0600

    Created DonationFormController (#12)
    Created Donation Form controller. No implementation or connections

---
+++ Controllers/DonationFormController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FundApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DonationFormController : ControllerBase
+    {
+        // GET: api/<DonationFormController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<DonationFormController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<DonationFormController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<DonationFormController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<DonationFormController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
