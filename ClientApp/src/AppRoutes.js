decf1f1ece1f5500a6f4f3a74b003dae305ae7ec
jsbd29 <30559771+jsbd29@users.noreply.github.com>
2023-02-27 19:40:33 -0600

    Update AppRoutes.js

--- ClientApp/src/AppRoutes.js
+++ ClientApp/src/AppRoutes.js
@@ -1,10 +1,10 @@
-import { Signup } from "./components/Signup";
-import { Login } from "./components/Login";
-import { Home } from "./components/Home";
-import { Fundraisers } from "./components/fundraiser/Fundraisers";
-import { Donate } from "./components/Donate";
-import { Dashboard } from "./components/user/Dashboard";
-import { CreateFundraisers } from "./components/fundraiser/CreateFundraiser";
+import { Signup } from './components/Signup';
+import { Login } from './components/Login';
+import { Home } from './components/Home';
+import { Fundraisers } from './components/fundraiser/Fundraisers';
+import { Donate } from './components/Donate';
+import { Dashboard } from './components/user/Dashboard';
+import { CreateFundraisers } from './components/fundraiser/CreateFundraiser';
 import { Settings } from "./components/Settings";
 
 const AppRoutes = [
