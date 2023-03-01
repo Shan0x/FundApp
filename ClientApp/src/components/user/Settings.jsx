ab73f98fcb61df5d06bcd1516c5420966da3828e
sebastiao dutra <30559771+jsbd29@users.noreply.github.com>
2023-02-28 20:15:41 -0600

    Settings page

---
+++ ClientApp/src/components/user/Settings.jsx
@@ -0,0 +1,21 @@
+/**
+ * @fileoverview User settings page to update profile details.
+ * @author
+ * */
+import React, { Component } from "react";
+
+export class Settings extends Component {
+  static displayName = Settings.name;
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Settings</h1>
+      </div>
+    );
+  }
+}
