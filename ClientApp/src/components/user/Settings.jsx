9a859b2dee85f260be9f84069ae102f24ca3d388
sebastiao dutra <30559771+jsbd29@users.noreply.github.com>
2023-03-03 20:17:45 -0600

    Edit profile form

--- ClientApp/src/components/user/Settings.jsx
+++ ClientApp/src/components/user/Settings.jsx
@@ -2,9 +2,10 @@
  * @fileoverview User settings page to update profile details.
  * @author
  * */
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 
-export class Settings extends Component {
+// Class Components
+/*export class Settings extends Component {
   static displayName = Settings.name;
 
   constructor(props) {
@@ -18,4 +19,57 @@ export class Settings extends Component {
       </div>
     );
   }
+}*/
+
+// Functional components
+export const Settings = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onDelete = () => {
+    console.log('delete')
+    // we need api route for deletion
+  }
+
+  const onUpdateProfile = (e) => {
+    e.preventDefault();
+    console.log('submit data', { username, password })
+    // we need api to submit data
+  }
+
+  // we need api to fetch user profile data
+
+  return (
+    <div>
+      <h1>Settings</h1>
+      <form onSubmit={onUpdateProfile}>
+        <div>
+          Username:
+          <input
+            placeholder="username"
+            type="text"
+            name="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div>
+          Password:
+          <input
+            placeholder="password"
+            type="text"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">Update Profile</button>
+      </form>
+
+      <div>Delete Your Account</div>
+      <button type="button" onClick={onDelete}>
+        Delete Account
+      </button>
+    </div>
+  );
 }
