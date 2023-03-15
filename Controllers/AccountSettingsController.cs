39ef3a12d0cb49da537aa9aec1df1a44154b2acf
crjpb3 <60162347+crjpb3@users.noreply.github.com>
2023-03-14 21:00:54 -0500

    Cris user settings update functionality (#19)
    * Added /api/accountsettings to setupProxy.js context object
    
    * Initial Implementation of User Password Change/Update
    
    * Implemented Update User Password and/or Email
    
    Implemented the functionality to update a user's password or update a user's email in the database
    
    * Added comments to my code
    
    ---------
    
    Co-authored-by: Shannen Lowe <lowes302@gmail.com>

--- Controllers/AccountSettingsController.cs
+++ Controllers/AccountSettingsController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using FundApp.Models;
+using Npgsql;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -8,6 +11,13 @@ namespace FundApp.Controllers
     [ApiController]
     public class AccountSettingsController : ControllerBase
     {
+        private readonly IConfiguration _configuration;
+
+        public AccountSettingsController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         // GET: api/<AccountSettingsController>
         [HttpGet]
         public IEnumerable<string> Get()
@@ -24,8 +34,53 @@ namespace FundApp.Controllers
 
         // POST api/<AccountSettingsController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        [Route("update/password")]
+        public bool updatePassword([FromBody] Users user)
         {
+            //Boolean to return to the frontend to inform it whether the query was successful or not
+            bool isQuerySuccess = true;
+
+//!!!!!The query building is currently vulnerable to injection since there is no input sanitization at this time!!!!!
+            //Building query with input from the frontend/user
+            NpgsqlConnection conn = new NpgsqlConnection(_configuration.GetConnectionString("localconnection").ToString());
+            NpgsqlCommand cmd = new NpgsqlCommand("UPDATE \"Users\" SET \"userPassword\"=\'" + user.userPassword + "\' WHERE \"userName\"=\'" + user.userName + "\';", conn);
+
+            conn.Open();
+            int queryExecutionStatus = cmd.ExecuteNonQuery();
+            conn.Close();
+
+            if (queryExecutionStatus == 0)
+            {//ExecuteNonQuery() returns 0 on a failed query
+                isQuerySuccess = false;
+            }
+
+            //If the expected body element is missing, isQuerySuccess is true when returned
+            return isQuerySuccess;
+        }
+
+        [HttpPost]
+        [Route("update/email")]
+        public bool updateEmail([FromBody] Users user)
+        {
+            //Boolean to return to the frontend to inform it whether the query was successful or not
+            bool isQuerySuccess = true;
+
+//!!!!!The query building is currently vulnerable to injection since there is no input sanitization at this time!!!!!
+            //Building query with input from the frontend/user
+            NpgsqlConnection conn = new NpgsqlConnection(_configuration.GetConnectionString("localconnection").ToString());
+            NpgsqlCommand cmd = new NpgsqlCommand("UPDATE \"Users\" SET \"userEmail\"=\'" + user.userEmail + "\' WHERE \"userName\"=\'" + user.userName + "\';", conn);
+
+            conn.Open();
+            int queryExecutionStatus = cmd.ExecuteNonQuery();
+            conn.Close();
+
+            if (queryExecutionStatus == 0)
+            {//ExecuteNonQuery() returns 0 on a failed query
+                isQuerySuccess = false;
+            }
+
+            //If the expected body element is missing, isQuerySuccess is true when returned
+            return isQuerySuccess;
         }
 
         // PUT api/<AccountSettingsController>/5
