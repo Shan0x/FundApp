41b1ef821cc3a49d9c7ca96aa56aabcca58c3e50
sebastiao dutra <30559771+jsbd29@users.noreply.github.com>
2023-03-15 09:07:34 -0500

    Added email and password update endpoints

--- ClientApp/src/components/user/Settings.jsx
+++ ClientApp/src/components/user/Settings.jsx
@@ -1,8 +1,15 @@
 /**
  * @fileoverview User settings page to update profile details.
  * @author
+ @SBD For the "settings" updates, I just have password and email updates implemented for now. The fetch URLs are "api/accountsettings/update/password" and "api/accountsettings/update/email". The only data that needs to be passed in the JSON body of the requests is:
+
+userName and userPassword(for a password update)
+userName and userEmail(for an email update)
+
+I have it return "true" if the update succeeds and "false" if the update fails.
  * */
 import React, { useState } from "react";
+import axios from 'axios'
 
 // Class Components
 /*export class Settings extends Component {
@@ -24,7 +31,9 @@ import React, { useState } from "react";
 // Functional components
 export const Settings = () =>
 {
-const [username, setUsername] = useState('');
+  const username = 'hardcoded'
+
+const [email, setEmail] = useState('');
 const [password, setPassword] = useState('');
 
 const onDelete = () =>
@@ -33,46 +42,50 @@ const onDelete = () =>
     // we need api route for deletion
   }
 
-  const onUpdateProfile = async (e) =>
+  const onUpdateEmail = async (e) =>
   {
       e.preventDefault();
-      console.log('submit data', { username, password });
-      try
-      {
-          const response = await fetch('api/accountsettings', {
-          method: 'POST', // PUT
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            username,
-            password
-          })
-        });
-
-        const data = await response.json();
-        console.log('data', data)
+      const payload = { username, email };
+      try  {
+        const response = await axios.post("api/accountsettings/update/email", payload);
+        console.log('response', response)
     } catch (error) {
       console.error(error)
      }
   }
-
+  const onUpdatePassword = async (e) =>
+  {
+      e.preventDefault();
+      const payload = { username, password };
+      try  {
+        const response = await axios.post('api/accountsettings/update/password', payload)
+        console.log('response', response)
+    } catch (error) {
+      console.error(error)
+     }
+  }
   // we need api to fetch user profile data
+
+  // how to start backend server
+  // how to get username
 
   return (
     < div >
       < h1 > Settings </ h1 >
-      < form onSubmit ={ onUpdateProfile}>
+      < form onSubmit ={ onUpdateEmail}>
         < div >
-          Username:
+          Email:
           < input
-            placeholder = "username"
+            placeholder = "Email"
             type = "text"
-            name = "username"
-            value ={ username}
-onChange ={ (e) => setUsername(e.target.value)}
+            name = "email"
+            value ={ email}
+onChange ={ (e) => setEmail(e.target.value)}
           />
         </ div >
+        <button type="submit">Update Email</button>
+      </form>
+       < form onSubmit ={ onUpdatePassword}>
         < div >
           Password:
           < input
@@ -83,7 +96,7 @@ onChange ={ (e) => setUsername(e.target.value)}
 onChange ={ (e) => setPassword(e.target.value)}
           />
         </ div >
-        < button type = "submit" > Update Profile </ button >
+        < button type = "submit" > Update Password </ button >
       </ form >
 
       < div > Delete Your Account</div>
