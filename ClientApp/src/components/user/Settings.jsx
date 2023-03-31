b010c18026fe99cc5a49e09309ee6c74083ab3e6
jsbd29 <30559771+jsbd29@users.noreply.github.com>
2023-03-31 09:05:26 -0500

    Update Settings.jsx
     ** No more console.log()
     ** updated delete warning
    ** password removed for email

--- ClientApp/src/components/user/Settings.jsx
+++ ClientApp/src/components/user/Settings.jsx
@@ -1,49 +1,9 @@
 /**
  * @format
  * @fileoverview User settings page to update profile details.
- * @author
-I updated the implementation for AccountSettingsController. There are 3 POST requests possible: update email, update password, and delete account.
-
-* notes backend from Cris
-The request's body format for an email update(request URL, "api/accountsettings/update/email"):
-"body":{
-  "userName": "<whatever their current username is>",
-  "userPassword": "<whatever their current password is>",
-  "userEmail": "<whatever their new/updated email is>"
-}
-
-
-The request's body format for a password update(request URL, "api/accountsettings/update/password"):
-"body":{
-  "userName": "<whatever their current username is>",
-  "userPassword": "<whatever their current password is>",
-  "newUserPassword": "<whatever their new/updated password is>"
-}
-
-
-The request's body format for account deletion(request URL, "api/accountsettings/update/delete"):
-"body":{
-  "userName": "<whatever their current username is>",
-  "userPassword": "<whatever their current password is>"
-}
- So @SBD if you look at my Signup.js
-I created a new object 
-const newUser = {
-    userName: data.get('username'),
-    ...
-    userEmail: data.get('email')
-};
-
-then used axios to create a post request to /api/Signup with the new object I created. 
-
-This is also within a function 'handleSubmit', which is called when they click the submit button.
-
-I imagine it will be a similar concept using the update settings api Cris created. You will just have to grab any missing data from the user session. 
-I think. Not 100% sure about it. That's just my thought process right now. (edited)
-
-1
-
- Notes from Shanne
+ * @author SBD
+ ** No more console.log()
+ ** updated delete warning
  */
 
 import React, { useState } from "react";
@@ -53,7 +13,7 @@ import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import Typography from "@mui/material/Typography";
-import { Box, TextField, Stack, Button } from "@mui/material/";
+import { DialogContentText, Box, TextField, Stack, Button } from "@mui/material/";
 import axios from "axios";
 import { UserInfo } from "./DonationInfo/UserInfo";
 
@@ -61,16 +21,24 @@ export const Settings = () => {
   const [newEmail, setNewEmail] = useState("");
   const [newPassword, setNewPassword] = useState("");
   const user = UserInfo();
-  console.log(user, "user");
 
   //added this to handle passworda and email
   const handleUpdate = () => {
-    const url = newEmail.includes("@") ? "/api/accountsettings/update/email" : "/api/accountsettings/update/password"
+    const url = newEmail.includes("@")
+      ? "/api/accountsettings/update/email"
+      : "/api/accountsettings/update/password";
     const payload = newEmail.includes("@")
-      ? { userName: user.userName,userPassword: user.userPassword , userEmail: newEmail }
-      : { userName: user.userName, userPassword: user.userPassword , newUserPassword: newPassword };
+      ? {
+          userName: user.userName,
+          userEmail: newEmail
+        }
+      : {
+          userName: user.userName,
+          userPassword: user.userPassword,
+          newUserPassword: newPassword
+        };
     axios
-      .post(url,payload)
+      .post(url, payload)
       .then((response) => {
         console.log(response.data);
         // show success message to user
@@ -80,7 +48,14 @@ export const Settings = () => {
         // show error message to user
       });
   };
+//added alert
+ const handleDelete = () => {
+    setOpenDialog(true);
+  };
 
+  const handleDialogClose = () => {
+    setOpenDialog(false);
+  };
   //added to delete account
   const handleDelete = () => {
     axios
@@ -96,6 +71,7 @@ export const Settings = () => {
         console.log(error);
         // show error message to user
       });
+         setOpenDialog(false);
   };
 
   return (
@@ -228,12 +204,31 @@ export const Settings = () => {
             columnGap={3}
             justifyContent='center'>
             <Button
-              variant='contained'
-              color='error'
-              sx={{ height: 40, borderRadius: "20px", width: 200 }}
-              onClick={handleDelete}>
-              Delete Account
-            </Button>
+        variant='contained'
+        color='error'
+        sx={{ height: 40, borderRadius: "20px", width: 200 }}
+        onClick={handleDelete}>
+        Delete Account
+      </Button>
+      <Dialog
+        open={openDialog}
+        onClose={handleDialogClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{"Delete Account"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Are you sure you want to delete your account?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleDialogClose}>Cancel</Button>
+          <Button onClick={handleDialogConfirm} autoFocus>
+            Delete
+          </Button>
+        </DialogActions>
+      </Dialog>
             <div>
               <Button
                 variant='contained'
@@ -254,3 +249,4 @@ export const Settings = () => {
     </>
   );
 };
+
