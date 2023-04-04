d6bdeae0eea7d547a1856aeeda6ae0ce64839586
crjpb3 <60162347+crjpb3@users.noreply.github.com>
2023-04-04 17:58:44 -0500

    Began Creating Fundraiser Data Model

---
+++ Models/Fundraisers.cs
@@ -0,0 +1,15 @@
+﻿using Azure.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace FundApp.Models
+{
+    public class Fundraisers
+    {
+        [Key]
+        public int fundraiserID { get; set; } = -1;
+        public int userID { get; set; } = -1;
+        public string fundraiserStatus { get; set; } = "ACTIVE";
+        public string fundraiserSummary { get; set; } = "N/A";
+        public double fundraiserGoalAmount { get; set; } = 0.00;
+    }
+}
