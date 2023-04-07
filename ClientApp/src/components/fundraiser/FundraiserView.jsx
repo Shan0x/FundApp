3c2419bd85f120d267b6406bdba4e9b64aa10800
Shannen <shannenmlowe@gmail.com>
2023-04-07 14:39:02 -0500

    Fundraiser views in progress.

---
+++ ClientApp/src/components/fundraiser/FundraiserView.jsx
@@ -0,0 +1,17 @@
+﻿/**
+ * @fileoverview Layout of individual fundraisers.
+ * 
+ * */
+import React from 'react';
+import {
+  Card,
+  CardContent,
+  Typography
+} from '@mui/material'
+
+const FundraiserItem = ({ fundraiser }) => {
+  return (
+    <Card>
+    </Card>
+    )
+}
---
+++ ClientApp/src/components/fundraiser/FundraisersList.js
@@ -0,0 +1,32 @@
+﻿/**
+ * @fileoverview List of all fundraisers created.
+ *
+ **/
+
+import React, { useState, useEffect } from 'react'
+import { Grid } from '@mui/material'
+import axios from 'axios';
+
+
+
+const FundraisersList = () => {
+  const [fundraisers, setFundraisers] = useState([]);
+
+  const getFundraisers = async () => {
+    // Fetch all fundraisers from database.
+    try {
+      const response = await axios.get('/api/fundraiser')
+      setFundraisers(response.data);
+    } catch (error) {
+      console.error('Error getting fundraisers: ', error);
+    }
+  };
+
+  useEffect(() => {
+    getFundraisers()
+  }, []);
+
+  const renderFundraiser = () => {
+    return 
+  }
+}
