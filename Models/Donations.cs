91c713b6817239379d613500d69eadde0ee131e9
Katzchii <phdcf@umsystem.edu>
2023-04-08 00:02:10 -0500

    Create Donation model

---
+++ Models/Donations.cs
@@ -0,0 +1,11 @@
+﻿namespace FundApp.Models
+{
+    public class Donations
+    {
+        public int? donationID { get; set; }
+        public int? fundraiserID { get; set; }
+        public double? dontaionAmount { get; set; }
+        public string? donatorName { get; set; }
+        public DateOnly? dontationDate { get; set; }
+    }
+}
